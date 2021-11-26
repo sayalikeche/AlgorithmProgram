e86c5dd532b3ea8e518e6afd98a94d46eb1c4d16
sayalikeche <kechesayali11@gmail.com>
2021-11-26 22:14:28 +0530

    [sayali added Algorithm Program]

---
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AlgorithmProgram");
+            Console.ReadLine();
+
+        }
+    }
+}
